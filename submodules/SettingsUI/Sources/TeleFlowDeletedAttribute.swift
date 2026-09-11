8033c9a6fd64f620c523bd03a1102979dd9f8966
urbanscooter <levaaa070@gmail.com>
2026-09-11 11:59:35 -0700

    Create TeleFlowDeletedAttribute.swift

---
+++ submodules/SettingsUI/Sources/TeleFlowDeletedAttribute.swift
@@ -0,0 +1,44 @@
+import Foundation
+import Postbox
+import TelegramCore
+
+/// Атрибут, вешается на сообщение после того, как собеседник его удалил.
+/// Сообщение остаётся в postbox, но помечено как "удалённое" — рендерится приглушённо.
+public final class TeleFlowDeletedAttribute: MessageAttribute {
+    public let deletedAt: Int32
+    public let originalAuthorName: String?
+
+    public init(deletedAt: Int32, originalAuthorName: String?) {
+        self.deletedAt = deletedAt
+        self.originalAuthorName = originalAuthorName
+    }
+
+    public init(decoder: PostboxDecoder) {
+        self.deletedAt = decoder.decodeInt32ForKey("d", orElse: 0)
+        self.originalAuthorName = decoder.decodeOptionalStringForKey("a")
+    }
+
+    public func encode(_ encoder: PostboxEncoder) {
+        encoder.encodeInt32(self.deletedAt, forKey: "d")
+        if let n = self.originalAuthorName {
+            encoder.encodeString(n, forKey: "a")
+        } else {
+            encoder.encodeNil(forKey: "a")
+        }
+    }
+
+    public var associatedPeerIds: [PeerId] { return [] }
+    public var associatedMessageIds: [MessageId] { return [] }
+}
+
+public extension Message {
+    var teleFlowIsDeleted: Bool {
+        return self.attributes.contains(where: { $0 is TeleFlowDeletedAttribute })
+    }
+}
+
+public extension EngineMessage {
+    var teleFlowIsDeleted: Bool {
+        return (self._asMessage()).teleFlowIsDeleted
+    }
+}
